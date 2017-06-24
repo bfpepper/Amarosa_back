aa2610d0c81edd922fa88842912ed78f638a3380
Ben Pepper <bfpepper87@gmail.com>
2017-06-24 16:15:42 -0600

    Initial commit with User tabel.

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    email "MyString"
+    password_digest "MyString"
+    gender "MyString"
+    name "MyString"
+    anniversary "2017-06-24 16:11:40"
+  end
+end
